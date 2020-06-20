afe8637dc96e91ac8093e56aeeb5a7f4fc366afb
aaron <achonculada@gmail.com>
2020-06-20 08:33:49 -0400

    chapter 1 hello world

---
+++ chapter1/hello_cargo/target/debug/deps/hello_cargo-21be69696ef6c434.d
@@ -0,0 +1,5 @@
+/home/aaron/Documents/rust-projects/the-rust-programming-language/chapter1/hello_cargo/target/debug/deps/hello_cargo-21be69696ef6c434: src/main.rs
+
+/home/aaron/Documents/rust-projects/the-rust-programming-language/chapter1/hello_cargo/target/debug/deps/hello_cargo-21be69696ef6c434.d: src/main.rs
+
+src/main.rs:
---
+++ chapter1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/aaron/Documents/rust-projects/the-rust-programming-language/chapter1/hello_cargo/target/debug/hello_cargo: /home/aaron/Documents/rust-projects/the-rust-programming-language/chapter1/hello_cargo/src/main.rs
