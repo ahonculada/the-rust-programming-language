d8a5a967d18f23b4a9dfa5b0f07b84d7497b2017
aaron <achonculada@gmail.com>
2020-06-30 01:11:05 -0400

    started strings

---
+++ chapter4/strings/target/debug/deps/strings-56896328920ce311.d
@@ -0,0 +1,5 @@
+/home/aaron/Documents/rust-projects/the-rust-programming-language/chapter4/strings/target/debug/deps/strings-56896328920ce311: src/main.rs
+
+/home/aaron/Documents/rust-projects/the-rust-programming-language/chapter4/strings/target/debug/deps/strings-56896328920ce311.d: src/main.rs
+
+src/main.rs:
---
+++ chapter4/strings/target/debug/strings.d
@@ -0,0 +1,1 @@
+/home/aaron/Documents/rust-projects/the-rust-programming-language/chapter4/strings/target/debug/strings: /home/aaron/Documents/rust-projects/the-rust-programming-language/chapter4/strings/src/main.rs
