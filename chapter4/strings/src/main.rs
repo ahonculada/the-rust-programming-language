6cb86a648fcdc8bf887082a81d84a62b16090234
aaron <achonculada@gmail.com>
2020-06-30 02:11:57 -0400

    examples done: move, clone, copy

--- chapter4/strings/src/main.rs
+++ chapter4/strings/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
 
-    let mut s = String::from("hello");
-    
-    s.push_str(", world!");
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
 
-    println!("{}", s);
+    println!("{}", s2);
+    
+    let x = 5;
+    let y = x;
+
+    println!("x = {}, y = {}", x, y);
 }
