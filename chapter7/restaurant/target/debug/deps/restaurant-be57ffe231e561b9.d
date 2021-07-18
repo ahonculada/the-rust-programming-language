8ca00cf82544c480827d1f4efbe47038fb3f02ec
ahonculada <achonculada@gmail.com>
2021-07-18 09:55:56 -0400

    package, crate, module organization

---
+++ chapter7/restaurant/target/debug/deps/restaurant-be57ffe231e561b9.d
@@ -0,0 +1,7 @@
+/home/corey/projects/rust/rust_book/chapter7/restaurant/target/debug/deps/restaurant-be57ffe231e561b9.rmeta: src/lib.rs
+
+/home/corey/projects/rust/rust_book/chapter7/restaurant/target/debug/deps/librestaurant-be57ffe231e561b9.rlib: src/lib.rs
+
+/home/corey/projects/rust/rust_book/chapter7/restaurant/target/debug/deps/restaurant-be57ffe231e561b9.d: src/lib.rs
+
+src/lib.rs:
---
+++ chapter7/restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+/home/corey/projects/rust/rust_book/chapter7/restaurant/target/debug/librestaurant.rlib: /home/corey/projects/rust/rust_book/chapter7/restaurant/src/lib.rs
